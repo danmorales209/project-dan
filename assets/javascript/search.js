c44e08d18f2502779da2f40bb05bfbeac9e6e8fb
Dan <danmorales209@gmail.com>
2019-02-06 20:18:29 -0800

    update files to master

--- assets/javascript/search.js
+++ assets/javascript/search.js
@@ -1,14 +1,3 @@
-// Initialize Firebase
-var config = {
-    apiKey: "AIzaSyCPuxUPfHfGN8dNwXc-K9h2j4fE5Re1nU0",
-    authDomain: "project-dan-a33a0.firebaseapp.com",
-    databaseURL: "https://project-dan-a33a0.firebaseio.com",
-    projectId: "project-dan-a33a0",
-    storageBucket: "project-dan-a33a0.appspot.com",
-    messagingSenderId: "151263152160"
-};
-firebase.initializeApp(config);
-var database = firebase.database();
 
 // Order recipes from most to least in terms of grams of the required ingredient
 function sortByWeight(ingredient, data) {
@@ -21,8 +10,6 @@ function sortByWeight(ingredient, data) {
 
         }
     }
-
-
 }
 
 $("#searchButton").on("click", function (e) {
